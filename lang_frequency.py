b38d0cb1fb97889eb2e9f9556911368c500e16d3
iriderpro <irider.pro@gmail.com>
2018-01-29 16:25:07 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,26 @@
+from collections import Counter
+import re
+import sys
+import os
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'rt', encoding='utf8') as file_obj:
+        list_words = []
+        for line in file_obj:
+            for word in re.findall(r'\w+', line):
+                list_words.append(word)
+        return list_words
 
 
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(list_words):
+    cntr_obj = Counter(list_words)
+    most_frequent_words = cntr_obj.most_common(10)
+    return most_frequent_words
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 1 or os.path.exists(sys.argv[1]) is False:
+        sys.exit('Не указан входной файл или он не существует')
+    frequent_words = get_most_frequent_words(load_data(sys.argv[1]))
+    print(frequent_words)
